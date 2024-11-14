9e0f886d85791c8b39558479a4f1b3f284dbd67d
Jaivin-hub <jaivinvenkolla@gmail.com>
2024-11-15 04:51:42 +0530

    ajax changes

--- src/js/fetchAndRenderData.js
+++ src/js/fetchAndRenderData.js
@@ -1,17 +1,21 @@
 import Mustache from 'mustache';
+import productList from '../assets/json/productlist.json';
 
 export default async function fetchAndRenderData(templateName, apiUrl, offset, limit, more) {
+    console.log('fetchAndRenderData000',fetchAndRenderData)
     try {
-        const response = await fetch(`${apiUrl}?limit=${limit}&offset=${offset}${more}`);
-        const data = await response.json();
-        console.log('data---->',data)
+        // const response = await fetch(`${apiUrl}?limit=${limit}&offset=${offset}${more}`);
+        // const data = await response.json();
+        // console.log('data---->',data)
         // Get the template source
-        const template = document.getElementById(templateName).innerHTML;
+        const template = document.getElementById('productlist-template').innerHTML;
+        console.log('productList-----',template)
 
         // Generate the HTML for all items
         let html = '';
-        data.list.forEach(item => {
+        productList.forEach(item => {
             html += Mustache.render(template, item);
+            console.log('html',html)
         });
         return html;
     } catch (error) {
